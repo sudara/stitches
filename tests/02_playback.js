864eebf918870e1724749ca99e2b5accb2a8433f
Sudara <sudara@alonetone.com>
2019-01-04 00:31:57 +0100

    test clicking somewhere then clicking play

--- tests/02_playback.js
+++ tests/02_playback.js
@@ -8,5 +8,17 @@ module.exports = {
       .pause(2000)
       .assert.containsText('#debug', 'playing: 2')
       .end()
+  },
+  'Clicking play on a track after clicking anywhere starts audio': (browser) => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('#logo', 500)
+      .click('#logo')
+      .waitForElementVisible('#debug', 500)
+      .click('a.track:nth-of-type(1)')
+      .assert.containsText('#debug', 'nodepool:create')
+      .pause(2000)
+      .assert.containsText('#debug', 'playing: 2')
+      .end()
   }
 }
