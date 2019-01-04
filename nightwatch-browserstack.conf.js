d4db216f50e5d132c9cdf2d1a566b3c451d61ba8
Sudara <sudara@alonetone.com>
2019-01-04 02:36:08 +0100

    Setup browserstack running on travis

--- nightwatch-browserstack.conf.js
+++ nightwatch-browserstack.conf.js
@@ -1,3 +1,5 @@
+var browserstack = require('browserstack-local');
+
 let nightwatch_config = {
   src_folders: ["tests"],
 
@@ -6,25 +8,75 @@ let nightwatch_config = {
     "host": "hub-cloud.browserstack.com",
     "port": 80
   },
+  common_capabilities: {
+    'browserstack.user': 'sudarawilliams1' || 'BROWSERSTACK_USERNAME',
+    'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+    'browserstack.debug': true,
+    'browserstack.local': true
+  },
 
   test_settings: {
     default: {
+      "launch_url": "http://localhost:8080"
+    },
+    chrome: {
       desiredCapabilities: {
-        'browserstack.user': 'sudarawilliams1',
-        'browserstack.key': 'BROWSERSTACK_ACCESS_KEY',
-        'browserstack.local': 'true',
-        'browserstack.debug': 'true',
-        'browser': 'chrome'
+        browser: "chrome"
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browser: "firefox"
+      }
+    },
+    safari: {
+      desiredCapabilities: {
+        'os_version': 'Mojave',
+        'browser': 'Safari',
+        'browser_version': '12.0',
+      }
+    },
+    edge: {
+      desiredCapabilities: {
+        'os': 'Windows',
+        'os_version': '10',
+        'browser': 'Edge',
+        'resolution': '1024x768'
+      }
+    },
+    iphone: {
+      desiredCapabilities: {
+        'device': 'iPhone 8 Plus',
+        'realMobile': 'true',
+        'os_version': '11.0'
+      }
+    },
+    galaxy: {
+      desiredCapabilities: {
+        'device': 'Samsung Galaxy S9',
+        'realMobile': 'true',
+        'os_version': '8.0'
+      }
+    },
+    pixel: {
+      desiredCapabilities: {
+        'device': 'Google Pixel 3',
+        'realMobile': 'true',
+        'os_version': '9.0'
       }
     }
   }
 };
 
-// Code to copy seleniumhost/port into test settings
-for(let i in nightwatch_config.test_settings){
-  const config = nightwatch_config.test_settings[i];
+// Code to support common capabilites
+for (var i in nightwatch_config.test_settings) {
+  var config = nightwatch_config.test_settings[i];
   config['selenium_host'] = nightwatch_config.selenium.host;
   config['selenium_port'] = nightwatch_config.selenium.port;
+  config['desiredCapabilities'] = config['desiredCapabilities'] || {};
+  for (var j in nightwatch_config.common_capabilities) {
+    config['desiredCapabilities'][j] = config['desiredCapabilities'][j] || nightwatch_config.common_capabilities[j];
+  }
 }
 
 module.exports = nightwatch_config;
