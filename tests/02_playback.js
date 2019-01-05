34c4c5370eb0ac30d767cd7cbcbc1e10e7b892a7
Sudara <sudara@alonetone.com>
2019-01-05 17:14:05 +0100

    Focus on testing just safari/ios until we get things sorted

--- tests/02_playback.js
+++ tests/02_playback.js
@@ -2,8 +2,8 @@ module.exports = {
   'Clicking play on a track starts audio': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('#debug', 1000)
-      .click('a.track:nth-of-type(1)')
+      .waitForElementVisible('#track1', 1000)
+      .click('#track1')
       .assert.containsText('#debug', 'nodepool:create')
       .pause(2000)
       .assert.containsText('#debug', 'playing: 2')
