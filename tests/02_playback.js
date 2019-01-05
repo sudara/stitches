faf74d52dd33422c1b03abe2379daff71f203f25
Sudara <sudara@alonetone.com>
2019-01-05 17:40:36 +0100

    Safari is unhappy waiting on element visible apparently

--- tests/02_playback.js
+++ tests/02_playback.js
@@ -2,7 +2,7 @@ module.exports = {
   'Clicking play on a track starts audio': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('#track1', 1000)
+      .waitForElementVisible('body')
       .click('#track1')
       .assert.containsText('#debug', 'nodepool:create')
       .pause(2000)
@@ -12,7 +12,7 @@ module.exports = {
   'Clicking play on a track after clicking anywhere starts audio': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('#logo', 1000)
+      .waitForElementVisible('body')
       .click('#logo')
       .waitForElementVisible('#debug', 1000)
       .click('a.track:nth-of-type(1)')
