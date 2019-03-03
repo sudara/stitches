2acf61eb7edbd52297d7b2d8dda23e94828f042a
Sudara <sudara@alonetone.com>
2019-03-03 23:26:46 +0100

    Use waitForElementPresent and not waitForElementVisible

--- tests/01_basics.js
+++ tests/01_basics.js
@@ -2,21 +2,21 @@ module.exports = {
   'Page Loads': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('body', 1000)
+      .waitForElementPresent('body', 1000)
       .assert.title('StitchES6')
       .end()
   },
   'StichES6 logs properly': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('#debug', 1000)
+      .waitForElementPresent('#debug', 1000)
       .assert.containsText('#debug','nodepool:create')
       .end()
   },
   'Clicking Anywhere unlocks audio': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('#logo', 1000)
+      .waitForElementPresent('#logo', 1000)
       .click('#logo')
       .assert.containsText('#debug', 'node:unlocked')
       .end()
--- tests/02_playback.js
+++ tests/02_playback.js
@@ -2,7 +2,7 @@ module.exports = {
   'Clicking play on a track starts audio': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('body')
+      .waitForElementPresent('body')
       .click('#track1')
       .assert.containsText('#debug', 'nodepool:create')
       .pause(2000)
@@ -12,9 +12,9 @@ module.exports = {
   'Clicking play on a track after clicking anywhere starts audio': (browser) => {
     browser
       .url(browser.launchUrl)
-      .waitForElementVisible('body')
+      .waitForElementPresent('body')
       .click('#logo')
-      .waitForElementVisible('#debug', 1000)
+      .waitForElementPresent('#debug', 1000)
       .click('a.track:nth-of-type(1)')
       .assert.containsText('#debug', 'nodepool:create')
       .pause(2000)
