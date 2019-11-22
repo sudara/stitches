0ded06c749559543ee4079fc12c581d11ba19a69
Sudara <sudara@alonetone.com>
2019-11-22 12:49:00 +0100

    Test macos on Catalina

--- nightwatch-browserstack.conf.js
+++ nightwatch-browserstack.conf.js
@@ -46,7 +46,7 @@ let nightwatch_config = {
     safari: {
       desiredCapabilities: {
         'os': 'OS X',
-        'os_version': 'Mojave',
+        'os_version': 'Catalina',
         'browser': 'Safari'
       }
     },
