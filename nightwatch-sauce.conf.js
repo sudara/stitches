e6236b3c05cd543b53531b0c99b4548c431cdf09
Sudara <sudara@alonetone.com>
2019-11-24 16:04:28 +0100

    Use ondemand.eu-central-1.saucelabs.com as the hostname

--- nightwatch-sauce.conf.js
+++ nightwatch-sauce.conf.js
@@ -14,8 +14,8 @@ let nightwatch_config = {
   test_settings: {
     default: {
       "launch_url": "http://localhost",
-      "selenium_port": process.env.SELENIUM_PORT || 80,
-      "selenium_host": process.env.SELENIUM_HOST || 'ondemand.saucelabs.com',
+      "selenium_port": process.env.SELENIUM_PORT || 4445,
+      "selenium_host": process.env.SELENIUM_HOST || 'ondemand.eu-central-1.saucelabs.com',
       "skip_testcases_on_fail": false,
     },
     chrome: {
