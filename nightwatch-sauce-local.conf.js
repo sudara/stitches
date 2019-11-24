c4d7c93d8cd0ac0cb1d95cdcf2c90bfc9c22f5f7
Sudara <sudara@alonetone.com>
2019-11-24 21:46:21 +0100

    Add a local nightwatch sauce config for testing purposes

---
+++ nightwatch-sauce-local.conf.js
@@ -0,0 +1,44 @@
+let nightwatch_config = {
+  src_folders: ["tests"],
+  custom_commands_path: "./nightwatch_commands",
+  selenium: {
+    "start_process": false,
+  },
+  common_capabilities: {
+    'javascriptEnabled': true,
+    "acceptSslCerts": true,
+  },
+
+  test_settings: {
+    default: {
+      "launch_url": "http://localhost:8080",
+      "skip_testcases_on_fail": false,
+      "selenium_host": "ondemand.eu-central-1.saucelabs.com",
+      "selenium_port": 4445,
+      "tunnelIdentifier": 'stitches-local',
+      "build": process.env.CIRCLE_BUILD_NUM,
+      "username": process.env.SAUCELABS_USER,
+      "access_key": process.env.SAUCELABS_KEY,
+      "use_ssl": false,
+      "output": true
+    },
+    chrome: {
+      desiredCapabilities: {
+        'platform': 'Windows 10',
+        'browserName': 'Chrome',
+        'version': 'latest'
+      }
+    },
+  }
+};
+
+// Code to support common capabilites
+for (var i in nightwatch_config.test_settings) {
+  var config = nightwatch_config.test_settings[i];
+  config['desiredCapabilities'] = config['desiredCapabilities'] || {};
+  for (var j in nightwatch_config.common_capabilities) {
+    config['desiredCapabilities'][j] = config['desiredCapabilities'][j] || nightwatch_config.common_capabilities[j];
+  }
+}
+
+module.exports = nightwatch_config;
