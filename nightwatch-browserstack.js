d66c566b02dca5331fb6051280cec3f2580ac4da
Sudara <sudara@alonetone.com>
2020-02-26 17:43:51 +0100

    Refactor browserstack status updating

--- nightwatch-browserstack.js
+++ nightwatch-browserstack.js
@@ -11,25 +11,33 @@ module.exports = {
   // to actually identify a failed test. Instead, we have to
   // issue an API request from a nightwatch callback to mark the test as failed
   // which will in turn mark it as failed on browserstack and eventually travis.
-  // Note that here we are just defining the function.
-  // We still need to call it from an afterEach hook in each "suite" (js file)
-  updateStatus (browser, done) {
-    const status = browser.currentTest.results.failed > 0 ? 'failed' : 'passed'
+  //
+  // Note: We still need to call it from an afterEach hook in each "suite" (js file)
+  // And not (as I spent way too much time figuring it out) just register this as
+  // a global afterEach
 
-    // See https://www.browserstack.com/automate/rest-api
-    request({
-      method: 'PUT',
-      uri: `https://api.browserstack.com/automate/sessions/${browser.sessionId}.json`,
-      auth: {
-        user: process.env.BROWSERSTACK_USERNAME,
-        pass: process.env.BROWSERSTACK_ACCESS_KEY,
-      },
-      form: {
-        "status": status,
-        "name": `${browser.currentTest.name}`
-      },
-    }, function (res, status, body) {
-        browser.end(function () { done() }
-    })
+  updateStatusIfBrowserstack (browser, done) {
+    // console.log('after each called....')
+    browser.perform(() => {
+      const status = browser.currentTest.results.failed > 0 ? 'failed' : 'passed'
+      console.log(`status was ${status} for ${browser.sessionId} running ${browser.currentTest.module}: ${browser.currentTest.name}`)
+
+      // only run if we are running in browserstack env
+      if (browser.launchUrl.includes("bs-local")) {
+        // See https://www.browserstack.com/automate/rest-api
+        request({
+          method: 'PUT',
+          uri: `https://api.browserstack.com/automate/sessions/${browser.sessionId}.json`,
+          auth: {
+            user: process.env.BROWSERSTACK_USERNAME,
+            pass: process.env.BROWSERSTACK_ACCESS_KEY,
+          },
+          form: {
+            "status": status,
+            "name": `${browser.currentTest.module}: ${browser.currentTest.name}`
+          },
+        })
+      }
+    }).end(() => done())
   }
 }
